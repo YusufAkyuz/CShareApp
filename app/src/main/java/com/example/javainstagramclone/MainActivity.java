253a1608d6a87703dca16551df88080ec8ad153b
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-07 20:30:34 +0300

    Codes were written for registration processes and layout updated

--- app/src/main/java/com/example/javainstagramclone/MainActivity.java
+++ app/src/main/java/com/example/javainstagramclone/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.javainstagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.javainstagramclone.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+
     }
 
     public void signInClicked(View view) {
@@ -19,6 +35,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signUpClicked(View view) {
+
+
+        String eMail = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+
+        if (eMail.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Enter Email and Password", Toast.LENGTH_LONG).show();
+        }else {
+            auth.createUserWithEmailAndPassword(eMail, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+
+
+
+
+
+
 
     }
 }
