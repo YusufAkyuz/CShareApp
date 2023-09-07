cfc2398aa398aa2e021d6b2ced19671be156a0f5
Yusuf Akyüz <yusufakyus47@gmail.com>
2023-09-07 17:42:58 +0300

    User interface written

--- app/src/main/java/com/example/javainstagramclone/FeedActivity.java
+++ app/src/main/java/com/example/javainstagramclone/FeedActivity.java
@@ -3,22 +3,12 @@ package com.example.javainstagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void signInClicked(View view) {
-
-    }
-
-    public void signUpClicked(View view) {
-
+        setContentView(R.layout.activity_feed);
     }
 }
